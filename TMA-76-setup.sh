00c756c4c944750d75186efdd42486a01efed2e3
Wiz <wiz@privatewizard.com>
2022-01-03 21:19:39 -0800

    rpm fusion updates

--- TMA-76-setup.sh
+++ TMA-76-setup.sh
@@ -2,6 +2,16 @@
 
 sudo hostname TMA-76
 
+## RPM Fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf groupupdate core
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
+sudo dnf install rpmfusion-free-release-tainted
+sudo dnf install libdvdcss
+sudo dnf install rpmfusion-nonfree-release-tainted
+sudo dnf install \*-firmware
+
 ## DNF Packages
 sudo dnf remove firefox -y # removing to make way for flatpak version
 sudo dnf update -y
